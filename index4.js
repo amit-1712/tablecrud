db358b464d4646a1b3aa47900b1b6a7354ce1378
com176 <amitpalsingh1712@gmail.com>
2022-02-09 20:31:46 +0530

    added

---
+++ index4.js
@@ -0,0 +1,112 @@
+const names = document.getElementById("names");
+const loc = document.getElementById("location");
+const company = document.getElementById("company");
+const items = document.getElementById("items");
+
+let targetElement;
+
+
+function submitFun() {
+
+    const n = names.value;
+    const o = loc.value;
+    const p = company.value;
+
+    if (targetElement) {
+        updateFun(targetElement,n,o,p);
+        targetElement = "";
+
+
+    }
+    else {
+
+
+        var item = createItem(n, o, p);
+
+        items.appendChild(item);
+
+    }
+
+}
+
+
+
+function updateFun(targetElement,n,o,p) {
+    targetElement.querySelector(".para1").innerHTML = n;
+    targetElement.querySelector(".para2").innerHTML = o;
+    targetElement.querySelector(".para3").innerHTML = p;
+    
+
+}
+
+
+
+function createItem(n, o, p) {
+
+
+    const divs = document.createElement("div");
+
+
+
+    const para1 = document.createElement("p");
+    para1.classList.add("para1");
+    para1.innerHTML = n
+    divs.appendChild(para1);
+
+    const para2 = document.createElement("p");
+    para2.classList.add("para2");
+    para2.innerHTML = o;
+
+    divs.appendChild(para2);
+
+
+    const para3 = document.createElement("p");
+    para3.classList.add("para3");
+    para3.innerHTML = p;
+    divs.appendChild(para3);
+
+
+
+    const deleteItem = document.createElement("button");
+
+    deleteItem.innerText = "delete";
+
+    divs.appendChild(deleteItem);
+
+    deleteItem.onclick = function () {
+
+        alert("deleted")
+
+        divs.remove();
+
+    }
+    names.value = "";
+    loc.value = "";
+    company.value = "";
+
+    const updateItem = document.createElement("button");
+    updateItem.innerText = "update";
+    divs.appendChild(updateItem);
+
+
+    updateItem.onclick = function () {
+
+        names.value = para1.innerHTML;
+        loc.value = para2.innerHTML;
+        company.value = para3.innerHTML;
+        targetElement = divs;
+    }
+
+
+
+    return divs;
+
+
+
+
+
+
+
+
+
+}
